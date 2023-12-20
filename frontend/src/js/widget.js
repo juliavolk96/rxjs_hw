594833e6df4b8bbffe0eedda80d7a516b4b05009
Julia <juliavolk96@gmail.com>
2023-12-20 21:41:22 +0400

    fix

--- frontend/src/js/widget.js
+++ frontend/src/js/widget.js
@@ -1,9 +1,6 @@
-import { doc } from 'prettier';
 import { ajax } from 'rxjs/ajax';
-import { interval } from 'rxjs/internal/observable/interval';
-import { catchError } from 'rxjs/internal/operators/catchError';
-import { switchMap } from 'rxjs/internal/operators/switchMap';
-import { of } from 'rxjs';
+import { interval, of } from 'rxjs';
+import { catchError, switchMap } from 'rxjs/operators';
 
 const POLLING_INTERVAL = 5000;
 const endpoint = 'http://localhost:3000/messages/unread';
@@ -12,7 +9,7 @@ const messages$ = interval(POLLING_INTERVAL).pipe(
   switchMap(() => ajax.getJSON(endpoint)),
   catchError(error => {
     console.error('Error:', error);
-    return of({messages: []});
+    throw new Error('Ошибка при получении сообщений. Пожалуйста, попробуйте еще раз.');
   })
 );
 
@@ -20,25 +17,31 @@ messages$.subscribe(({ messages }) => {
   messages.forEach(addMessageToTable);
 });
 
+function formatDate(dateString) {
+  const date = new Date(dateString);
+  const hours = date.getHours().toString().padStart(2, '0');
+  const minutes = date.getMinutes().toString().padStart(2, '0');
+  const formattedDate = new Intl.DateTimeFormat('ru-RU').format(date);
+  return `${formattedDate} ${hours}:${minutes}`;
+}
+
+function createCell(text) {
+  const cell = document.createElement('td');
+  cell.setAttribute('role', 'cell');
+  cell.textContent = text;
+  return cell;
+}
+
 function addMessageToTable(message) {
   const table = document.getElementById('root');
   const row = document.createElement('tr');
-  const idCell = document.createElement('td');
-  const fromCell = document.createElement('td');
-  const subjectCell = document.createElement('td');
-  const bodyCell = document.createElement('td');
-  const receivedCell = document.createElement('td');
+  row.setAttribute('role', 'row'); 
+  
+  const fromCell = createCell(message.from);
+  const subjectCell = createCell(message.subject.length > 15 ? message.subject.slice(0, 15) + '...' : message.subject);
+  const bodyCell = createCell(message.body);
+  const receivedCell = createCell(formatDate(message.received));
 
-  idCell.textContent = message.id;
-  fromCell.textContent = message.from;
-  subjectCell.textContent = message.subject.length > 15 ? message.subject.slice(0, 15) + '...' : message.subject;
-  bodyCell.textContent = message.body;
-
-  const date = new Date(message.received);
-  const formattedDate = `${date.getHours()}:${date.getMinutes()}${date.getDate()}.${date.getMonth() + 1}.${date.getFullYear()}`;
-  receivedCell.textContent = formattedDate;
-
-  row.appendChild(idCell);
   row.appendChild(fromCell);
   row.appendChild(subjectCell);
   row.appendChild(bodyCell);
